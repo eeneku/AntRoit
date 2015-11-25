1c49a4443f05d4fbf3d088b3ae412be274717d72
eeneku <eeneku@gmail.com>
2015-11-25 18:09:34 +0200

    Added gml and cleaned code.

--- AntRoit/jni/AntRoit.cpp
+++ AntRoit/jni/AntRoit.cpp
@@ -26,47 +26,72 @@
 #include <stdlib.h>
 #include <math.h>
 
+#include <vector>
+#include <glm\glm.hpp>
+
+#pragma region GL debug
+
 #define  LOG_TAG    "libgl2jni"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
 
-static void printGLString(const char *name, GLenum s) {
+static void printGLString(const char *name, GLenum s) 
+{
 	const char *v = (const char *)glGetString(s);
 	LOGI("GL %s = %s\n", name, v);
 }
 
-static void checkGlError(const char* op) {
+static void checkGlError(const char* op) 
+{
 	for (GLint error = glGetError(); error; error
 		= glGetError()) {
 		LOGI("after %s() glError (0x%x)\n", op, error);
 	}
 }
 
-static const char gVertexShader[] =
-"attribute vec4 vPosition;\n"
-"void main() {\n"
-"  gl_Position = vPosition;\n"
+#pragma endregion
+
+#pragma region Shaders
+
+static const char vertexShader[] =
+"attribute vec4 position;\n"
+"void main()\n"
+"{\n"
+"	gl_Position = position;\n"
 "}\n";
 
-static const char gFragmentShader[] =
+static const char fragmentShader[] =
 "precision mediump float;\n"
-"void main() {\n"
-"  gl_FragColor = vec4(0.5, 0.5, 0.5, 1.0);\n"
+"void main()\n"
+"{\n"
+"	gl_FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);\n"
 "}\n";
 
-GLuint loadShader(GLenum shaderType, const char* pSource) {
+GLuint program;
+GLuint positionHandle;
+
+#pragma endregion
+
+#pragma region Shader loading
+
+GLuint loadShader(GLenum shaderType, const char* pSource) 
+{
 	GLuint shader = glCreateShader(shaderType);
-	if (shader) {
+	if (shader)
+	{
 		glShaderSource(shader, 1, &pSource, NULL);
 		glCompileShader(shader);
 		GLint compiled = 0;
 		glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-		if (!compiled) {
+		if (!compiled) 
+		{
 			GLint infoLen = 0;
 			glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
-			if (infoLen) {
+			if (infoLen)
+			{
 				char* buf = (char*)malloc(infoLen);
-				if (buf) {
+				if (buf) 
+				{
 					glGetShaderInfoLog(shader, infoLen, NULL, buf);
 					LOGE("Could not compile shader %d:\n%s\n",
 						shaderType, buf);
@@ -80,19 +105,17 @@ GLuint loadShader(GLenum shaderType, const char* pSource) {
 	return shader;
 }
 
-GLuint createProgram(const char* pVertexSource, const char* pFragmentSource) {
-	GLuint vertexShader = loadShader(GL_VERTEX_SHADER, pVertexSource);
-	if (!vertexShader) {
-		return 0;
-	}
+GLuint createProgram(const char* vertexSource, const char* fragmentSource) 
+{
+	GLuint vertexShader = loadShader(GL_VERTEX_SHADER, vertexSource);
+	if (!vertexShader) return 0;
 
-	GLuint pixelShader = loadShader(GL_FRAGMENT_SHADER, pFragmentSource);
-	if (!pixelShader) {
-		return 0;
-	}
+	GLuint pixelShader = loadShader(GL_FRAGMENT_SHADER, fragmentSource);
+	if (!pixelShader) return 0;
 
 	GLuint program = glCreateProgram();
-	if (program) {
+	if (program) 
+	{
 		glAttachShader(program, vertexShader);
 		checkGlError("glAttachShader");
 		glAttachShader(program, pixelShader);
@@ -100,12 +123,15 @@ GLuint createProgram(const char* pVertexSource, const char* pFragmentSource) {
 		glLinkProgram(program);
 		GLint linkStatus = GL_FALSE;
 		glGetProgramiv(program, GL_LINK_STATUS, &linkStatus);
-		if (linkStatus != GL_TRUE) {
+		if (linkStatus != GL_TRUE)
+		{
 			GLint bufLength = 0;
 			glGetProgramiv(program, GL_INFO_LOG_LENGTH, &bufLength);
-			if (bufLength) {
+			if (bufLength) 
+			{
 				char* buf = (char*)malloc(bufLength);
-				if (buf) {
+				if (buf) 
+				{
 					glGetProgramInfoLog(program, bufLength, NULL, buf);
 					LOGE("Could not link program:\n%s\n", buf);
 					free(buf);
@@ -118,72 +144,137 @@ GLuint createProgram(const char* pVertexSource, const char* pFragmentSource) {
 	return program;
 }
 
-GLuint gProgram;
-GLuint gvPositionHandle;
+#pragma endregion
 
-bool setupGraphics(int w, int h) {
+#pragma region Shapes
+
+const float rectangle[] =
+{
+	0.0f, 1.0f,
+	1.0f, 1.0f,
+	0.0f, 0.0f,
+
+	1.0f, 1.0f,
+	1.0f, 0.0f,
+	0.0f, 0.0f
+};
+
+const float triangle[] =
+{
+	0.0f, 1.0f,
+	1.0f, 1.0f,
+	0.0f, 0.0f
+};
+
+struct Shape
+{
+	virtual void draw() = 0;
+
+protected:
+	glm::vec4 color;
+	float x;
+	float y;
+};
+
+struct Triangle : public Shape
+{
+	void draw()
+	{
+		glVertexAttribPointer(positionHandle, 2, GL_FLOAT, GL_FALSE, 0, triangle);
+		checkGlError("glVertexAttribPointer");
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		checkGlError("glDrawArrays");
+	}
+};
+
+struct Rectangle : public Shape
+{
+	void draw()
+	{
+		glVertexAttribPointer(positionHandle, 2, GL_FLOAT, GL_FALSE, 0, rectangle);
+		checkGlError("glVertexAttribPointer");
+
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		checkGlError("glDrawArrays");
+	}
+};
+
+std::vector<Shape*> shapes;
+
+#pragma endregion
+
+#pragma region Init
+
+void init(int width, int height)
+{
 	printGLString("Version", GL_VERSION);
 	printGLString("Vendor", GL_VENDOR);
 	printGLString("Renderer", GL_RENDERER);
 	printGLString("Extensions", GL_EXTENSIONS);
 
-	LOGI("setupGraphics(%d, %d)", w, h);
-	gProgram = createProgram(gVertexShader, gFragmentShader);
-	if (!gProgram) {
-		LOGE("Could not create program.");
-		return false;
-	}
-	gvPositionHandle = glGetAttribLocation(gProgram, "vPosition");
-	checkGlError("glGetAttribLocation");
-	LOGI("glGetAttribLocation(\"vPosition\") = %d\n",
-		gvPositionHandle);
+	LOGI("setupGraphics(%d, %d)", width, height);
 
-	glViewport(0, 0, w, h);
+	program = createProgram(vertexShader, fragmentShader);
+	if (!program)
+	{
+		LOGE("Could not create program.");
+		return;
+	}
+
+	positionHandle = glGetAttribLocation(program, "position");
+	checkGlError("glGetAttribLocation");
+	LOGI("glGetAttribLocation(\"position\") = %d\n", positionHandle);
+
+	glViewport(0, 0, width, height);
 	checkGlError("glViewport");
-	return true;
+
+	glClearColor(0.7f, 0.3f, 0.1f, 1.0f);
+	checkGlError("glClearColor");
 }
 
-const GLfloat gTriangleVertices[] = { 
-	-0.5f, 0.5f,
-	0.5f, 0.5f,
-	-0.5f, -0.5f,
+#pragma endregion
 
-	0.5f, 0.5f, 
-	0.5f, -0.5f,
-	-0.5f, -0.5f };
+#pragma region Update
 
-void renderFrame() {
-	static float grey;
-	grey += 0.01f;
-	if (grey > 1.0f) {
-		grey = 0.5f;
-	}
-	glClearColor(grey, grey, grey, 1.0f);
-	checkGlError("glClearColor");
+void update()
+{
+}
 
+#pragma endregion
+
+#pragma region Draw
+
+void draw()
+{
 	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
 	checkGlError("glClear");
 
-	glUseProgram(gProgram);
+	glUseProgram(program);
 	checkGlError("glUseProgram");
 
-	glVertexAttribPointer(gvPositionHandle, 2, GL_FLOAT, GL_FALSE, 0, gTriangleVertices);
-	checkGlError("glVertexAttribPointer");
-	glEnableVertexAttribArray(gvPositionHandle);
-	checkGlError("glEnableVertexAttribArray");
-	glDrawArrays(GL_TRIANGLES, 0, 6);
-	checkGlError("glDrawArrays");
+	for (auto shape : shapes)
+	{
+		shape->draw();
+	}
 }
+
+#pragma endregion
+
+#pragma region Java
 
 extern "C"
 {
 	JNIEXPORT void JNICALL Java_fi_enko_antroit_AntRoitLib_init(JNIEnv * env, jobject obj, jint width, jint height)
 	{
-		setupGraphics(width, height);
+		init(width, height);
 	}
 
 	JNIEXPORT void JNICALL Java_fi_enko_antroit_AntRoitLib_step(JNIEnv * env, jobject obj)
 	{
-		renderFrame();
+		update();
+		draw();
 	}
 }
+
+#pragma endregion
